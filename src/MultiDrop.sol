400c8fc911da9f77066a7353a8e832eea24069af
Prajjawalk <prajjawalkhandelwal@gmail.com>
2024-09-21 18:45:18 +0800

    upgrade multidrop

--- src/MultiDrop.sol
+++ src/MultiDrop.sol
@@ -5,12 +5,13 @@ import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
 
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
-import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolKey, Currency} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
-import "@uniswap/v4-core/contracts/interfaces/IUniswapV4Pool.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {stdMath} from "forge-std/stdMath.sol";
+import {console} from "forge-std/console.sol";
 
 contract MultiDrop is BaseHook {
     using PoolIdLibrary for PoolKey;
@@ -26,7 +27,7 @@ contract MultiDrop is BaseHook {
     // mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
     // mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
     // Mapping to store tokenB balances before the swap for each user
-    mapping(address => uint256) private preSwapBalances;
+    // mapping(address => uint256) public preSwapBalances;
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -34,16 +35,16 @@ contract MultiDrop is BaseHook {
         return Hooks.Permissions({
             beforeInitialize: false,
             afterInitialize: false,
-            beforeAddLiquidity: true,
+            beforeAddLiquidity: false,
             afterAddLiquidity: false,
-            beforeRemoveLiquidity: true,
+            beforeRemoveLiquidity: false,
             afterRemoveLiquidity: false,
-            beforeSwap: true,
+            beforeSwap: false,
             afterSwap: true,
             beforeDonate: false,
             afterDonate: false,
             beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
+            afterSwapReturnDelta: true,
             afterAddLiquidityReturnDelta: false,
             afterRemoveLiquidityReturnDelta: false
         });
@@ -53,81 +54,77 @@ contract MultiDrop is BaseHook {
     // NOTE: see IHooks.sol for function documentation
     // -----------------------------------------------
 
-    function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata swapParams, bytes calldata hookData)
-        external
-        override
-        returns (bytes4, BeforeSwapDelta, uint24)
-    {
-        // beforeSwapCount[key.toId()]++;
-        (bool isOneMany, address[] receivers) = abi.decode(hookData, (bool, address[]));
-        if(isOneMany) {
+    // function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata swapParams, bytes calldata hookData)
+    //     external
+    //     override
+    //     returns (bytes4, BeforeSwapDelta, uint24)
+    // {
+    //     // beforeSwapCount[key.toId()]++;
+    //     // (bool isOneMany, address[] memory receivers) = abi.decode(hookData, (bool, address[]));
+    //     // if(isOneMany) {
           
-          require(receivers.length > 0);
+    //     //   require(receivers.length > 0);
           
-          if(swapParams.zeroForOne) {
-            address token1 = key.currency1;
-            preSwapBalances[sender] = IERC20(token1).balanceOf(sender);
-          } else {
-            address token0 = key.currency0;
-            preSwapBalances[sender] = IERC20(token0).balanceOf(sender);
-          }
-        }
+    //     //   if(swapParams.zeroForOne) {
+    //     //     Currency token1 = key.currency1;
+    //     //     preSwapBalances[sender] = token1.balanceOf(sender);
+    //     //   } else {
+    //     //     Currency token0 = key.currency0;
+    //     //     preSwapBalances[sender] = token0.balanceOf(sender);
+    //     //   }
+    //     // }
 
         
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    //     return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    // }
+
+
+    struct MyData {
+      int128 amount;
+      uint256 postSwapBalance;
+      uint256 amountPerAddress;
+      Currency toSend;
     }
 
-    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata hookData)
+    function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata swapParams, BalanceDelta delta, bytes calldata hookData)
         external
         override
         returns (bytes4, int128)
     {
         // afterSwapCount[key.toId()]++;
-        (bool isOneMany, address[] receivers) = abi.decode(hookData, (bool, address[]));
-        uint256 amount;
+        (bool isOneMany, address sender, address[] memory receivers) = abi.decode(hookData, (bool, address, address[]));
+        
+        MyData memory data = MyData({
+          amount: 0,
+          postSwapBalance: 0,
+          amountPerAddress: 0,
+          toSend: Currency.wrap(address(0))
+        });
+
         if(isOneMany) {
           // beforeSwapCount[key.toId()]++;
           if(swapParams.zeroForOne) {
-            address token1 = key.currency1;
-            uint256 postSwapBalance = IERC20(token1).balanceOf(sender);
-            amount = postSwapBalance - preSwapBalances[sender];
-            uint amountPerAddress = amount/receivers.length;
-
-            for (uint i = 0; i < receivers.length; i++) {
-              IERC20(token1).transferFrom(sender, receivers[i]);
-            }
+            data.toSend = (key.currency1);
+            data.amount = delta.amount1();
           } else {
-            address token0 = key.currency0;
-            uint256 postSwapBalance = IERC20(token0).balanceOf(sender);
-            amount = postSwapBalance - preSwapBalances[sender];
+            data.toSend = key.currency0;
+            data.amount = delta.amount0();
+          }
 
-            uint amountPerAddress = amount/receivers.length;
-
-            for (uint i = 0; i < receivers.length; i++) {
-              IERC20(token1).transferFrom(sender, receivers[i]);
+          
+          data.amountPerAddress = stdMath.abs(data.amount)/receivers.length;
+          
+          for (uint i = 0; i < receivers.length; i++) {
+            if (i == receivers.length - 1) {
+              poolManager.take(data.toSend, receivers[i], uint256(stdMath.abs(data.amount)) - data.amountPerAddress * (receivers.length - 1));
+            } else {
+              poolManager.take(data.toSend, receivers[i], data.amountPerAddress);
             }
           }
+
+          // poolManager.take(data.toSend, sender,stdMath.abs(data.amount));
+          // poolManager.settle();
         }
-        return (BaseHook.afterSwap.selector, 0);
-    }
-
-    function beforeAddLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeAddLiquidityCount[key.toId()]++;
-        return BaseHook.beforeAddLiquidity.selector;
-    }
-
-    function beforeRemoveLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) external override returns (bytes4) {
-        beforeRemoveLiquidityCount[key.toId()]++;
-        return BaseHook.beforeRemoveLiquidity.selector;
+        return (BaseHook.afterSwap.selector, data.amount); // positive number
     }
 }
---
+++ test/MultiDrop.t.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+import {MultiDrop} from "../src/MultiDrop.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {EasyPosm} from "./utils/EasyPosm.sol";
+import {Fixtures} from "./utils/Fixtures.sol";
+import {console} from "forge-std/console.sol";
+
+contract MultiDropTest is Test, Fixtures {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
+
+    MultiDrop hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    int24 tickLower;
+    int24 tickUpper;
+
+    function setUp() public {
+        // creates the pool manager, utility routers, and test tokens
+        deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
+
+        deployAndApprovePosm(manager);
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(
+                Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
+            ) ^ (0x4444 << 144));
+        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
+        deployCodeTo("MultiDrop.sol:MultiDrop", constructorArgs, flags);
+        hook = MultiDrop(flags);
+
+        // Create the pool
+        key = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        poolId = key.toId();
+        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+
+        // Provide full-range liquidity to the pool
+        tickLower = TickMath.minUsableTick(key.tickSpacing);
+        tickUpper = TickMath.maxUsableTick(key.tickSpacing);
+
+        uint128 liquidityAmount = 100e18;
+
+        (uint256 amount0Expected, uint256 amount1Expected) = LiquidityAmounts.getAmountsForLiquidity(
+            SQRT_PRICE_1_1,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            liquidityAmount
+        );
+
+        (tokenId,) = posm.mint(
+            key,
+            tickLower,
+            tickUpper,
+            liquidityAmount,
+            amount0Expected + 1,
+            amount1Expected + 1,
+            address(this),
+            block.timestamp,
+            ZERO_BYTES
+        );
+    }
+
+    /// @notice Helper function for a simple ERC20 swaps that allows for unlimited price impact
+    function swapMod(PoolKey memory _key, bool zeroForOne, int256 amountSpecified, bytes memory hookData, int256 inputBaseValue)
+        internal
+        returns (BalanceDelta)
+    {
+        // allow native input for exact-input, guide users to the `swapNativeInput` function
+        bool isNativeInput = zeroForOne && _key.currency0.isAddressZero();
+        if (isNativeInput) require(0 > amountSpecified, "Use swapNativeInput() for native-token exact-output swaps");
+
+        uint256 value = isNativeInput ? uint256(stdMath.abs( inputBaseValue - amountSpecified)) : stdMath.abs(inputBaseValue);
+
+        return swapRouter.swap{value: value}(
+            _key,
+            IPoolManager.SwapParams({
+                zeroForOne: zeroForOne,
+                amountSpecified: amountSpecified,
+                sqrtPriceLimitX96: zeroForOne ? MIN_PRICE_LIMIT : MAX_PRICE_LIMIT
+            }),
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}),
+            hookData
+        );
+    }
+
+    function testMultiDropHooks() public {
+        // Perform a test swap //
+        bool zeroForOne = true;
+        int256 amountSpecified = -9e18; // negative number indicates exact input swap!
+        address testReceiver = vm.addr(2);
+        address[] memory addrs = new address[](3);
+        addrs[0] = testReceiver;
+        addrs[1] = vm.addr(3);
+        addrs[2] = vm.addr(4);
+        bytes memory swapdata = abi.encode(true, vm.addr(1), addrs);
+
+        BalanceDelta swapDelta = swapMod(key, zeroForOne, amountSpecified, swapdata, 1e10);
+        // console.log("swap delta is ", swapDelta.amount1());
+
+        // ------------------- //
+        assertEq(int256(swapDelta.amount0()), amountSpecified);
+        console.log("balance of testReceiver is: ", currency1.balanceOf(vm.addr(1)));
+        assertEq(currency1.balanceOf(vm.addr(1)), 0);
+    }
+}
